995996a73dc3c54da55d109c449435f629a55be9
Scott Rutzmoser <scott_rutzmoser@yahoo.com>
2018-09-11 16:00:39 -0400

    Intial Commit
    initial commit

---
+++ data.js
@@ -0,0 +1,232 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lgnc-parking-lot",
+      "name": "LGNC Parking Lot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 0.1680736771436333,
+        "pitch": -0.09147522821700171,
+        "fov": 1.421094961189048
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.945761407916205,
+          "pitch": 0.2765164350113274,
+          "rotation": 0,
+          "target": "1-lgnc_360_0034"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7251714903225626,
+          "pitch": -0.132609372753123,
+          "title": "LGNC Education Center",
+          "text": "The Lehigh Gap Nature Center is a member-supported 501(c)3 non-profit conservation organization located in Lehigh Gap, PA, at the foot of the Kittatinny Ridge.<br><div><br></div><div><br></div>"
+        },
+        {
+          "yaw": -2.071501074440235,
+          "pitch": -0.23801564825797072,
+          "title": "Drone Footage",
+          "text": "<iframe width='220' height='124' src='https://www.youtube.com/embed/hKgloFPwPbA' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>"
+        }
+      ]
+    },
+    {
+      "id": "1-lgnc_360_0034",
+      "name": "LGNC_360_0034",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 2.4661929486904857,
+        "pitch": -0.01746556807600541,
+        "fov": 1.421094961189048
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8757623976529239,
+          "pitch": 0.017612240764858456,
+          "rotation": 0,
+          "target": "0-lgnc-parking-lot"
+        },
+        {
+          "yaw": -2.3812041269993784,
+          "pitch": 0.13089894217711517,
+          "rotation": 0,
+          "target": "2-lgnc_360_0035"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-lgnc_360_0035",
+      "name": "LGNC_360_0035",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6055276345961342,
+          "pitch": 0.02523006814890394,
+          "rotation": 0,
+          "target": "1-lgnc_360_0034"
+        },
+        {
+          "yaw": -2.647554144392764,
+          "pitch": 0.2591003373661209,
+          "rotation": 0,
+          "target": "3-lgnc_360_0037"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lgnc_360_0037",
+      "name": "LGNC_360_0037",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.13213107670111413,
+          "pitch": 0.10005740072713287,
+          "rotation": 0,
+          "target": "2-lgnc_360_0035"
+        },
+        {
+          "yaw": -2.8054094806644407,
+          "pitch": 0.11030082079968295,
+          "rotation": 0,
+          "target": "4-lgnc_360_0038"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-lgnc_360_0038",
+      "name": "LGNC_360_0038",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -3.1386743545310374,
+        "pitch": 0.12482613397816777,
+        "fov": 1.421094961189048
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.33726905904077853,
+          "pitch": 0.03236804142697203,
+          "rotation": 0,
+          "target": "3-lgnc_360_0037"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
